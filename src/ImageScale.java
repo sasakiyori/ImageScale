b04e7381cede684c4d2a580611d33a558307d9fe
sasakiyori <sasakiyori@172.19.99.99>
2017-09-21 16:00:07 +0800

    complete the bit-depth problem

--- src/ImageScale.java
+++ src/ImageScale.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
 import java.awt.image.RenderedImage;
 import java.io.Console;
 import java.io.File;
@@ -7,10 +8,13 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+import org.omg.CORBA.PUBLIC_MEMBER;
+
 public class ImageScale {
 	public int[][] getImage() {
 		try {
 			BufferedImage inputImage = ImageIO.read(new File("05.png"));
+			
 			int width = inputImage.getWidth();
 			int height = inputImage.getHeight();
 			
@@ -18,17 +22,15 @@ public class ImageScale {
 			
 			for (int y = 0; y < height; y++)
 				for (int x = 0; x < width; x++) {
-					imageMatrix[x][y] = inputImage.getRaster().getSample(x, y, 0);
+					//imageMatrix[x][y] = inputImage.getRaster().getSample(x, y, 0);
 					
 					//imageMatrix[x][y] = inputImage.getRGB(x, y);
 					
-					/*
 					int rgb = inputImage.getRGB(x, y);
 					int r = (rgb >> 16) & 0xFF;
 					int g = (rgb >> 8) & 0xFF;
 					int b = rgb & 0xFF;
 					imageMatrix[x][y] = (int)(0.3 * r + 0.59 * g + 0.11 * b);
-					*/
 				}
 			return imageMatrix;
 		} catch (IOException e) {
@@ -37,19 +39,18 @@ public class ImageScale {
 		}
 	}
 	
-	public void printImage(int[][] imageData) {
+	public void printImage(int[][] imageData, String oper) {
 		if (imageData != null) {
 			int width = imageData.length;
 			int height = imageData[0].length;
 			
-			BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_BYTE_GRAY);
 			for (int i = 0; i < height; i++)
 				for (int j = 0; j < width; j++) {
 					result.setRGB(j, i, new Color(imageData[j][i], imageData[j][i], imageData[j][i]).getRGB());
-					//result.getRaster().setDataElements(j, i, imageData[j][i]);
 				}
 			try {
-				File resultImg = new File(width + "-" + height + ".png");
+				File resultImg = new File(oper + "-" + width + "-" + height + ".png");
 				ImageIO.write((RenderedImage)result, "png", resultImg);
 			} catch (Exception e) {
 				// TODO: handle exception
@@ -73,10 +74,20 @@ public class ImageScale {
 
 				// the formula from wikipedia
 				// x1 = a, x2 = a + 1, y1 = b, y2 = b + 1
-				int fxy = (int)((b + 1 - y)*((a + 1 - x)*originImage[a][b] + (x - a)*originImage[a+1][b]) + 
-						(y - b)*((a + 1 - x)*originImage[a][b + 1] + (x - a)*originImage[a + 1][b + 1]));
+				int fxy = (int)((b + 1 - y) * ((a + 1 - x) * originImage[a][b] + (x - a) * originImage[a + 1][b]) + 
+						(y - b) * ((a + 1 - x) * originImage[a][b + 1] + (x - a) * originImage[a + 1][b + 1]));
 				outputImage[i][j]= fxy;
 			}
 		return outputImage;
 	}
+	
+//	public int[][] quantize(int[][] originImage, int level) {
+//		int width = originImage.length;
+//		int height = originImage[0].length;
+//		int[][] quantizedImage = new int[width][height];
+//		// the distance from each level
+//		int d = 255 / (level - 1);
+//		
+//	}
+
 }
--- src/ImageScaleRunner.java
+++ src/ImageScaleRunner.java
@@ -4,11 +4,24 @@ public class ImageScaleRunner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ImageScale imageScale = new ImageScale();
-		// first problem
-		//imageScale.printImage(imageScale.scale(imageScale.getImage(), 192, 128));
-		//imageScale.printImage(imageScale.scale(imageScale.getImage(), 96, 64));
-		//imageScale.printImage(imageScale.scale(imageScale.getImage(), 48, 32));
-		//imageScale.printImage(imageScale.scale(imageScale.getImage(), 24, 16));
-		imageScale.printImage(imageScale.scale(imageScale.getImage(), 400, 300));
+		// 2.2 first problem
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 192, 128), "scale");
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 96, 64), "scale");
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 48, 32), "scale");
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 24, 16), "scale");
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 12, 8), "scale");
+		// 2.2 second problem
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 300, 200), "scale");
+		// 2.2 third problem
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 450, 300), "scale");
+		// 2.2 fourth problem
+		imageScale.printImage(imageScale.scale(imageScale.getImage(), 500, 200), "scale");
+		
+		// 2.3
+//		imageScale.printImage(imageScale.quantize(imageScale.getImage(), 128), "quantize128");
+//		imageScale.printImage(imageScale.quantize(imageScale.getImage(), 32), "quantize32");
+//		imageScale.printImage(imageScale.quantize(imageScale.getImage(), 8), "quantize8");
+//		imageScale.printImage(imageScale.quantize(imageScale.getImage(), 4), "quantize4");
+//		imageScale.printImage(imageScale.quantize(imageScale.getImage(), 2), "quantize2");
 	}
 }
